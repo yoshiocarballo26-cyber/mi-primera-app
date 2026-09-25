f7f971011b3fe854a4daa31663d1b085cd8b75bd
Administrador <yoshiocarballo26@gmail.com>
2026-09-24 23:38:58 -0600

    Mejorar Editandroid: selector de archivos y navegación WebView

--- app/src/main/java/com/yc/editor3d/android/MainActivity.kt
+++ app/src/main/java/com/yc/editor3d/android/MainActivity.kt
@@ -2,7 +2,10 @@ package com.yc.editor3d.android
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
@@ -10,6 +13,8 @@ import android.webkit.WebViewClient
 
 class MainActivity : Activity() {
     private lateinit var web: WebView
+    private var fileCallback: ValueCallback<Array<Uri>>? = null
+    private val fileRequestCode = 4103
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +26,34 @@ class MainActivity : Activity() {
             domStorageEnabled = true
             databaseEnabled = true
             allowFileAccess = true
+            allowContentAccess = true
             cacheMode = WebSettings.LOAD_DEFAULT
             mediaPlaybackRequiresUserGesture = false
         }
         web.webViewClient = WebViewClient()
-        web.webChromeClient = WebChromeClient()
+        web.webChromeClient = object : WebChromeClient() {
+            override fun onShowFileChooser(
+                view: WebView?,
+                callback: ValueCallback<Array<Uri>>?,
+                params: FileChooserParams?
+            ): Boolean {
+                fileCallback?.onReceiveValue(null)
+                fileCallback = callback
+
+                val intent = params?.createIntent() ?: Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                    addCategory(Intent.CATEGORY_OPENABLE)
+                    type = "*/*"
+                }
+                return try {
+                    startActivityForResult(intent, fileRequestCode)
+                    true
+                } catch (_: Exception) {
+                    fileCallback?.onReceiveValue(null)
+                    fileCallback = null
+                    false
+                }
+            }
+        }
         setContentView(web)
 
         if (savedInstanceState == null) {
@@ -35,12 +63,31 @@ class MainActivity : Activity() {
         }
     }
 
+    @Deprecated("Use Activity Result APIs when the Android shell grows beyond the prototype.")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode != fileRequestCode) return
+        val result = if (resultCode == RESULT_OK && data?.data != null) {
+            arrayOf(data.data!!)
+        } else {
+            null
+        }
+        fileCallback?.onReceiveValue(result)
+        fileCallback = null
+    }
+
+    override fun onBackPressed() {
+        if (web.canGoBack()) web.goBack() else super.onBackPressed()
+    }
+
     override fun onSaveInstanceState(outState: Bundle) {
         web.saveState(outState)
         super.onSaveInstanceState(outState)
     }
 
     override fun onDestroy() {
+        fileCallback?.onReceiveValue(null)
+        fileCallback = null
         web.destroy()
         super.onDestroy()
     }
